76e2df8c4bbf0c9a7c6c70410889d5c4fb8bf38d
omotowa oluwafemi <oluwafemisamuel2004@gmail.com>
2025-02-23 18:13:38 +0100

    fixed cta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -250,7 +250,7 @@ export default function Home() {
       <Navbar />
 
       {/* Hero Section */}
-      <section className="relative px-4 h-[100dvh] bg-gradient-to-r from-gray-900 to-black overflow-hidden">
+      <section className="relative px-2 lg:px-4 h-[100dvh] bg-gradient-to-r from-gray-900 to-black overflow-hidden">
         <div className="absolute inset-0 bg-black/50" />
         <div className="container mx-auto px-4 h-full flex items-center relative z-10">
           <div className="max-w-2xl space-y-8">
@@ -336,12 +336,14 @@ export default function Home() {
               </div>
             ))}
           </div>
+          <div className="flex w-full justify-center mt-6 items-center">
           <Link
                 href="/inventory"
-                className="bg-gradient-to-r from-gray-900 to-black overflow-hidden text-white px-4 py-2 lg:px-8 lg:py-4 text-[14px] lg:text-[16px] rounded-md font-semibold hover:bg-gray-100 transition-colors flex items-center"
+                className="bg-gradient-to-r w-fit  from-gray-900 to-black overflow-hidden text-white px-4 py-2 lg:px-8 lg:py-4 text-[14px] lg:text-[16px] rounded-md font-semibold hover:bg-gray-100 transition-colors flex items-center"
               >
                 View Our Collection
               </Link>
+              </div>
         </div>
       </section>
 
@@ -463,7 +465,7 @@ export default function Home() {
       </section>
 
       {/* Footer */}
-      <footer className="bg-black px-4 text-white py-12">
+      <footer className="bg-black px-2 lg:px-4 text-white py-12">
         <div className="container mx-auto px-4">
           <div className="grid md:grid-cols-4 gap-8">
             <div>
